159d6c1281589f17572f6e16b5289e901481ead4
hellozo0 <ahju0011@gmail.com>
2021-11-08 02:17:46 +0900

    ➕[ADD] : 게시글 View UI (#3)

---
+++ Carrot_Market_iOS/Carrot_Market_iOS/View Controllers/Item/ItemDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  ItemDetailVC.swift
+//  Carrot_Market_iOS
+//
+//  Created by 안현주 on 2021/11/08.
+//
+
+import UIKit
+
+class ItemDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
